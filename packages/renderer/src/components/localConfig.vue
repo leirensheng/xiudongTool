e73b94ba15eee07ceb38d4c94bd57c21d2c3178a
leirensheng <leirensheng@163.com>
2023-03-28 16:20:35 +0800

    feat: 服务管理

--- packages/renderer/src/components/localConfig.vue
+++ packages/renderer/src/components/localConfig.vue
@@ -29,8 +29,9 @@
         class="show-ip"
         @click="copyText(config.dnsIp + ':5678')"
       >
-        <el-icon class="copy-icon"> <DocumentCopy /> </el-icon>{{ config.dnsIp }}:5678</span
-      >
+        <el-icon class="copy-icon">
+          <DocumentCopy />
+        </el-icon>{{ config.dnsIp }}:5678</span>
       <el-button
         :loading="loadingDns"
         @click="refreshDns"
@@ -53,19 +54,35 @@
         @exit="isStart = false"
       ></cmd-terminal2>
     </div>
+    <div class="server-info">
+      <div
+        v-for="(item, index) in items"
+        :key="index"
+        class="row"
+      >
+        <div class="show">{{ item.name }}_{{ item.showTime }}: </div>
+        <el-input
+          v-model="item.ip"
+          class="val"
+        ></el-input>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import {readFile, writeFile, copyText} from '#preload';
-import {ElNotification} from 'element-plus';
-import {getIp} from '../utils/index.js';
+import { readFile, writeFile, copyText } from '#preload';
+import { ElNotification } from 'element-plus';
+import { getIp } from '../utils/index.js';
 export default {
   data() {
     return {
       isStart: false,
       loadingDns: false,
       config: {},
+      items: [
+
+      ],
     };
   },
   created() {
@@ -86,7 +103,7 @@ export default {
     async handleMessage(val) {
       if (val.includes('成功')) {
         let dnsIp = val.match(/\[(.*?)\]/);
-        await writeFile('localConfig.json', JSON.stringify({...this.config, dnsIp}, null, 4));
+        await writeFile('localConfig.json', JSON.stringify({ ...this.config, dnsIp }, null, 4));
         ElNotification({
           title: '成功',
           message: 'WAN ip更新成功',
@@ -104,18 +121,55 @@ export default {
       this.getConfig();
       this.loadingDns = false;
     },
+    getServerInfo() {
+      return this.items.filter(one => one.ip).reduce((prev, cur) => {
+        let key = cur.name + '_' + cur.activityId;
+        prev[key] = cur.ip;
+        return prev;
+      }, {});
+    },
     async onSubmit() {
-      await writeFile('localConfig.json', JSON.stringify(this.config, null, 4));
+      let serverInfo = this.getServerInfo();
+
+
+      await writeFile('localConfig.json', JSON.stringify({
+        ...this.config,
+        serverInfo,
+      }, null, 4));
       ElNotification({
         title: '成功',
         message: '保存成功',
         type: 'success',
       });
+      this.getConfig();
+
     },
     async getConfig() {
       let str = await readFile('localConfig.json');
       let config = JSON.parse(str);
       this.config = config;
+      let info = config.serverInfo;
+      let hasConfigActivities = [];
+      let savedData = Object.entries(info).map(([key, ip]) => {
+        let [name, activityId] = key.split('_');
+        hasConfigActivities.push(activityId);
+        return {
+          name,
+          activityId,
+          ip,
+        };
+      });
+
+
+      let allConfig = await readFile('config.json');
+      allConfig = JSON.parse(allConfig);
+
+      let noSaveData = Object.values(allConfig).map(one => ({ name: one.activityName, activityId: one.activityId, ip: '', showTime: one.showTime })).filter(one => ![hasConfigActivities].includes(one.activityId));
+
+      this.items = [
+        ...savedData,
+        ...noSaveData,
+      ];
     },
   },
 };
@@ -129,6 +183,26 @@ export default {
     position: relative;
     top: 2px;
     margin-right: 10px;
+  }
+}
+
+.server-info {
+  margin-top: 15px;
+  height: 200px;
+  overflow-y: auto;
+  .row {
+    display: flex;
+    align-items: center;
+    line-height: 2.5;
+    .show {
+      overflow: hidden;
+      text-align: right;
+      padding-right: 15px;
+    }
+
+    .val {
+      width: 300px;
+    }
   }
 }
 </style>
