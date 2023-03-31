8aee672fac21f6d71a29eb3057527dda76a301aa
leirensheng <leirensheng@163.com>
2023-03-31 15:45:01 +0800

    feat: 去重

--- packages/renderer/src/components/configManage.vue
+++ packages/renderer/src/components/configManage.vue
@@ -21,6 +21,9 @@
         <el-form-item label="show">
           <el-switch v-model="isShow"></el-switch>
         </el-form-item>
+        <el-form-item label="去重">
+          <el-switch v-model="isUnique"></el-switch>
+        </el-form-item>
       </el-form>
     </div>
 
@@ -38,7 +41,7 @@
       :on-dialog-open="onDialogOpen"
       @before-assign-to-table="beforeAssignToTable"
     >
-      <template #username="{row}">
+      <template #username="{ row }">
         <div>
           <el-dropdown trigger="contextmenu">
             <span class="el-dropdown-link">
@@ -88,7 +91,7 @@
           </el-dropdown>
         </div>
       </template>
-      <template #activityId="{row}">
+      <template #activityId="{ row }">
         <div>
           <el-icon
             class="copy-icon"
@@ -99,7 +102,7 @@
           <span>{{ row.activityId }}</span>
         </div>
       </template>
-      <template #activityName="{row}">
+      <template #activityName="{ row }">
         <div>
           <el-icon
             class="copy-icon"
@@ -110,7 +113,7 @@
           <span>{{ row.activityName }}</span>
         </div>
       </template>
-      <template #targetTypes="{row}">
+      <template #targetTypes="{ row }">
         <el-tag
           v-for="(item, i) in row.targetTypes"
           :key="item"
@@ -169,14 +172,14 @@
 </template>
 
 <script>
-import {readFile, readDir, cmd, copyText, writeFile, getComputerName, getRemoteIp} from '#preload';
-import {ElMessageBox} from 'element-plus';
-import {useStore} from '/@/store/global';
+import { readFile, readDir, cmd, copyText, writeFile, getComputerName, getRemoteIp } from '#preload';
+import { ElMessageBox } from 'element-plus';
+import { useStore } from '/@/store/global';
 import CmdTerminal2 from './cmdTerminal2.vue';
 import axios from 'axios';
-import {ElNotification} from 'element-plus';
-import {getIp} from '/@/utils/index.js';
-import {storeToRefs} from 'pinia';
+import { ElNotification } from 'element-plus';
+import { getIp } from '/@/utils/index.js';
+import { storeToRefs } from 'pinia';
 import CalcUser from '/@/components/calcUser.vue';
 import RecoverState from '/@/components/recoverState.vue';
 export default {
@@ -187,7 +190,7 @@ export default {
   },
   setup() {
     let store = useStore();
-    let {pidInfo} = storeToRefs(store);
+    let { pidInfo } = storeToRefs(store);
 
     let useServer = () => {
       let startServer = () => {
@@ -210,6 +213,7 @@ export default {
   },
   data() {
     return {
+      isUnique: false,
       tableData: [],
       pcName: '',
       pcs: ['新电脑', '虚拟机4.3', '虚拟机4.4', '联想'],
@@ -254,8 +258,8 @@ export default {
           name: 'isSuccess',
           isShow: false,
           options: [
-            {name: '是', id: true},
-            {name: '否', id: false},
+            { name: '是', id: true },
+            { name: '否', id: false },
           ],
           support: {
             query: {
@@ -268,7 +272,7 @@ export default {
           name: 'user',
           width: 100,
           valueType: 'slot',
-          rules: [{validator: this.validateUser, trigger: 'blur'}],
+          rules: [{ validator: this.validateUser, trigger: 'blur' }],
           support: {
             query: {},
             add: {},
@@ -408,8 +412,8 @@ export default {
             },
           },
           options: [
-            {id: true, name: '是'},
-            {id: false, name: '否'},
+            { id: true, name: '是' },
+            { id: false, name: '否' },
           ],
         },
 
@@ -428,11 +432,14 @@ export default {
   },
   computed: {
     title() {
-      let {activityName, username, showTime} = this.curRow;
+      let { activityName, username, showTime } = this.curRow||{};
       return `${username}__${activityName}__${showTime}`;
     },
   },
   watch: {
+    isUnique(){
+      this.getList();
+    },
     isHideFre() {
       this.getList();
     },
@@ -478,7 +485,7 @@ export default {
       delete this.pidInfo[this.cmd];
       this.getList();
     },
-    tableRowClassName({row, rowIndex}) {
+    tableRowClassName({ row, rowIndex }) {
       if (row.remark && row.remark.includes('频繁')) {
         return 'grey';
       }
@@ -499,7 +506,7 @@ export default {
       let config = obj[this.curRow.username];
       let res = await axios.post(
         'http://127.0.0.1:4000/copyUserFile',
-        {username: this.curRow.username, host: getRemoteIp(this.remotePc), config},
+        { username: this.curRow.username, host: getRemoteIp(this.remotePc), config },
         {
           timeout: 20000,
         },
@@ -535,7 +542,7 @@ export default {
       console.log(11111, row);
       this.remoteDialogVisible = true;
     },
-    beforeAssignToTable({records}) {
+    beforeAssignToTable({ records }) {
       this.tableData = records;
     },
     getStyle(row) {
@@ -550,13 +557,18 @@ export default {
     getList() {
       return this.$refs.table.getList();
     },
-    async copy({username}) {
-      let {value} = await ElMessageBox.prompt('', '输入新用户');
-      let {value: phone} = await ElMessageBox.prompt('', '用户手机号');
+    async copy({ username }) {
+      let { value } = await ElMessageBox.prompt('', '输入新用户');
+      let { value: phone } = await ElMessageBox.prompt('', '用户手机号');
       this.loading = true;
       await this.cmdCopy(value, username, phone);
+      this.isUnique = false;
+
+      this.$refs.table.resetQuery(true);
       await this.getList();
       this.loading = false;
+
+
       let target = this.tableData.find(one => one.username === value);
       this.start(target);
     },
@@ -599,13 +611,13 @@ export default {
       row.status = 1;
     },
     async handlerAdd(val) {
-      await this.updateFile({key: val.username, val, isAdd: true});
+      await this.updateFile({ key: val.username, val, isAdd: true });
       await this.getList();
       let target = this.tableData.find(one => one.username === val.username);
       this.start(target);
     },
     async handleEdit(val) {
-      let obj = {...val};
+      let obj = { ...val };
       delete obj.ticketTypes;
       delete obj.username;
       await this.updateFile({
@@ -614,7 +626,7 @@ export default {
       });
       await this.$refs.table.getList();
     },
-    async updateFile({key, val, isAdd}) {
+    async updateFile({ key, val, isAdd }) {
       let fileData = await this.getConfigFile();
       if (isAdd && fileData[key] !== undefined) {
         throw new Error('已经有了' + key);
@@ -624,7 +636,7 @@ export default {
     },
     async onDialogOpen(form) {
       let target = this.items.find(one => one.id === 'targetTypes');
-      target.options = (form.ticketTypes || []).map(one => ({id: one, name: one}));
+      target.options = (form.ticketTypes || []).map(one => ({ id: one, name: one }));
       return form;
     },
     async remove(obj, noShowConfirm) {
@@ -665,7 +677,7 @@ export default {
         }
       });
     },
-    async getData({queryItems}) {
+    async getData({ queryItems }) {
       let obj = await this.getConfigFile();
       let data = Object.entries(obj).map(([key, val]) => ({
         ...val,
@@ -675,7 +687,7 @@ export default {
 
       let items = queryItems.filter(item => item.value);
       data = data.filter(one => {
-        return items.every(({value, column}) => String(one[column]).indexOf(value) !== -1);
+        return items.every(({ value, column }) => String(one[column]).indexOf(value) !== -1);
       });
       data.sort((a, b) => new Date(b.port) - new Date(a.port));
 
@@ -690,10 +702,16 @@ export default {
         this.isHideFre ? !(one.remark && one.remark.includes('频繁')) : true,
       );
       this.stopFrequency(data);
-      this.tableData = data;
+
+      if (this.isUnique) {
+        let activityIds = [... new Set(data.map(one =>Number( one.activityId)))];
+        this.tableData = activityIds.map(activityId=>data.find(one => Number(one.activityId) === activityId));
+      } else {
+        this.tableData = data;
+      }
       return {
-        total: data.length,
-        records: data,
+        total: this.tableData.length,
+        records: this.tableData,
       };
     },
   },
